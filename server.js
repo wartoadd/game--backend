87e380e64926df9b7a3733ba734c50325e2fc6c9
wartoadd <wartoadd@gmail.com>
2026-07-21 21:00:00 -0400

    Refactor server.js to include CORS and JSON middleware

--- server.js
+++ server.js
@@ -9,4 +9,12 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
-});
+
+  import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+})
